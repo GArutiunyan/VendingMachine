adf60a8cb29980887df00a0cd4bcfa3964e6be2f
GArutiunyan <cololor65@gmail.com>
2023-04-08 14:55:57 +0300

    added shutdownhook to save changes before termination

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
 import controller.*;
-
 public class Main {
+    private static class SaveMyMapDBToFile extends Thread {
+        @Override
+        public void run() {
+            Controller.saveMyMapDBToFile();
+        }
+    }
     public static void main(String[] args) {
+        SaveMyMapDBToFile saveMyMapDBToFile = new SaveMyMapDBToFile();
+        Runtime.getRuntime().addShutdownHook(saveMyMapDBToFile);
         Controller.loadMyMapDB();
         while (true){
             Controller.loginScreen();
             if(!Controller.isLoggedIn()){
-                Controller.saveMyMapDBToFile();
-                return;
+                System.exit(0);
             }
             Controller.mainMenu();
         }
